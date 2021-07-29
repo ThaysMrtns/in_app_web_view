1809f83161bd68d82ca6518cfe1904d6147e2772
ThaysMrtns <thays.martins@nave.org.br>
2021-07-29 15:05:30 -0300

    bottom bar plugin e view web page

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:bottom_bar/bottom_bar.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'dart:io';
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (Platform.isAndroid) {
     await AndroidInAppWebViewController.setWebContentsDebuggingEnabled(true);
   }
-  
-  runApp(MyApp());
+
+  runApp(
+    MaterialApp(home: new MyApp())
+  );
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'In app web view'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+class MyApp extends StatefulWidget {
+  final MyAppInBrowser browser = new MyAppInBrowser();
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyAppState extends State<MyApp> {
+  var options = InAppBrowserClassOptions(
+    crossPlatform: InAppBrowserOptions(hideUrlBar: false),
+    inAppWebViewGroupOptions: InAppWebViewGroupOptions(crossPlatform: InAppWebViewOptions(javaScriptEnabled: true))
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("App web view"),
       ),
       body: Center(
-        child: Text("ola mundo")
+        child: ElevatedButton(
+          child: Text("Busca inicial"),
+          onPressed: (){
+            widget.browser.openUrlRequest(
+              urlRequest: URLRequest(url: Uri.parse("https://www.livima.com.br/busca_inicial")),
+              options: options
+            );
+          },
+        )
+      ),
+      bottomNavigationBar: BottomBar(
+        backgroundColor: Colors.blue[50],
+        items: [
+          BottomBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+            activeColor: Colors.blue,
+          ),
+          BottomBarItem(
+            icon: Icon(Icons.favorite),
+            title: Text('Favorites'),
+            activeColor: Colors.red,
+            darkActiveColor: Colors.red.shade400, // Optional
+          ),
+          BottomBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Account'),
+            activeColor: Colors.greenAccent.shade700,
+            darkActiveColor: Colors.greenAccent.shade400, // Optional
+          ),
+          BottomBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+            activeColor: Colors.orange,
+          ),
+        ],
       ),
     );
+  }
+}
+
+class MyAppInBrowser extends InAppBrowser{
+  //Funções dos estados
+  @override
+  Future onBrowserCreated() async {
+    print("Browser Created!");
+  }
+
+  @override
+  Future onLoadStart(url) async {
+    print("Started $url");
+  }
+
+  @override
+  Future onLoadStop(url) async {
+    print("Stopped $url");
+  }
+
+  @override
+  void onLoadError(url, code, message) {
+    print("Can't load $url.. Error: $message");
+  }
+
+  @override
+  void onProgressChanged(progress) {
+    print("Progress: $progress");
+  }
+
+  @override
+  void onExit() {
+    print("Browser closed!");
   }
 }
